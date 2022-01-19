67afe170eb84766a1d3acf23cace1b9bc7120e59
GuillaumeGetkoz <48527673+GuillaumeGetkoz@users.noreply.github.com>
2022-01-19 13:30:14 +0100

    Stay peachy!

--- episodes.js
+++ episodes.js
@@ -1,6 +1,4 @@
-/*
-const episodes = {
-			S2E08: [`A Lot Of Sparkles`, ['Tidyping', 'MJ'], `Dylan encounters MJ, a famous basketball player, and asks for an autograph. But Tidyping, who wants to clean everything, makes the autograph mysteriously disappear...`, `<video controls poster="thumbnails/S2E08.png"><source src="https://onedrive.live.com/download?cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211259&authkey=AEzj-CN2CVYPVwE" type="video/mp4" /></video>`],
+/* 			S2E08: [`A Lot Of Sparkles`, ['Tidyping', 'MJ'], `Dylan encounters MJ, a famous basketball player, and asks for an autograph. But Tidyping, who wants to clean everything, makes the autograph mysteriously disappear...`, `<video controls poster="thumbnails/S2E08.png"><source src="https://onedrive.live.com/download?cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211259&authkey=AEzj-CN2CVYPVwE" type="video/mp4" /></video>`],
 
 			S2E09: [`Yummyping Is The Best Cooking Teacher`, ['Yummyping'], `After the chef quits, the business isn't going well at Eline's Seasoned Chicken Restaurant. To help her mother, Ellie practices day and night, but keeps failing over and over again. But Yummyping, who smells the chicken, decides to help her.`, `<video controls poster="thumbnails/S2E09.png"><source src="https://onedrive.live.com/download?cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211260&authkey=AOtT26Y8smwiCXg" type="video/mp4" /></video>`],
 			
@@ -21,10 +19,72 @@ const episodes = {
 			S2E17: [`The Jewel Teeniepings Got Stolen!`, ['Thieves', 'Jewelry Box'], `Sneak peek - While everyone is sleeping, two thieves enter the bakery to steal the Jewelry Box and the Jewel Teeniepings! And when Romi wakes up, it's already too late...`, `<video controls poster="thumbnails/S2E17.png"><source src="https://onedrive.live.com/download?cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211268&authkey=AI7YvfC2dLyVT64" type="video/mp4" /></video>`],
 
 			S2E18: [`Who Is The Real Heartsping?`, ['Buddyping'], `Trailer - Buddyping is having fun duplicating everything in the bakery. And when Heartsping tries to find him, she gets duplicated into her twin... Good luck finding who's the real one!`, `<iframe src="https://www.youtube.com/embed/UXiSrLPIGIk" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`]
-	}
-*/
+ */
 
 const episodes = {
+	S2E01: {
+		title: "Sparkling Jewel Teenieping",
+		description: "While Romi is having peaceful days in Harmony Village, Gamyeonping released all the Jewel Teeniepings to Earth. To save them, Joahping, Teeheeping and Trustping decide to go...",
+		wikiPages: ["Jewel Teeniepings", "Jewel Heart Wing Phone", "Joahping", "Teeheeping", "Trustping"],
+		specialStatus: "",
+		videoType: "YouTube",
+		videoUrl: "_Trni5YhFUw",
+		onedriveUrl: ""
+	},
+	S2E02: {
+		title: "Teenieping Academy Open!",
+		description: "Romi comes in contact with Dr. Monju. She will have to catch all the Jewel Teeniepings, with the 3 new students Teeniepings. And to form the newbies, Dr. Monju has prepared a little gift!",
+		wikiPages: ["Teenieping Academy", "Jewelry Box"],
+		specialStatus: "",
+		videoType: "YouTube",
+		videoUrl: "jKAXq4IJHv8",
+		onedriveUrl: ""
+	},
+	S2E03: {
+		title: "We Have To Put Up With the Tickling!",
+		description: "Tickleping is having fun tickling everyone of Harmony Village. Romi must catch her, but before, the Teeniepings must train to put up with the tickling!",
+		wikiPages: ["Tickleping"],
+		specialStatus: "",
+		videoType: "YouTube",
+		videoUrl: "alEFGDOSu60",
+		onedriveUrl: ""
+	},
+	S2E04: {
+		title: "Ayaya Stop Hurting Me",
+		description: "Trustping enjoys Ian's new chocolates so much that he eats way too many of them, and gets a bad toothache the very next day! And Owwping, who enters the bakery, makes things even worse!",
+		wikiPages: ["Owwping"],
+		specialStatus: "",
+		videoType: "YouTube",
+		videoUrl: "5Nf5Gt6ThUU",
+		onedriveUrl: ""
+	},
+	S2E05: {
+		title: "Wishping's Three Wishes",
+		description: "Lena, Romi's greatest enemy, discovers Wishping. To thank her, Wishping decides to grant Lena 3 wishes. But when Romi finds her, it's too late: Lena refuses to give up Wishping...",
+		wikiPages: ["Wishping"],
+		specialStatus: "",
+		videoType: "YouTube",
+		videoUrl: "WDY_SuqaN3Q",
+		onedriveUrl: ""
+	},
+	S2E06: {
+		title: "Pat Pat I'll Give You Warm Comfort!",
+		description: "Kyle is devastated when he learns that he failed to enter the international pastry school, and decides to leave. Joahping decides to follow him, when she suddenly runs into her friend Patping...",
+		wikiPages: ["Patping"],
+		specialStatus: "",
+		videoType: "YouTube",
+		videoUrl: "QUUXGhWLoDc",
+		onedriveUrl: ""
+	},
+	S2E07: {
+		title: "Disappeared Romi",
+		description: "Gamyeonping is in Harmony Village! And to make things worse, while Heartsping is searching them, Timidping turns Romi and her friends into super small persons!",
+		wikiPages: ["Timidping", "Gamyeonping"],
+		specialStatus: "",
+		videoType: "YouTube",
+		videoUrl: "ezbVkqn_Yrs",
+		onedriveUrl: ""
+	},
 	S2E08: {
 		title: "A Lot Of Sparkles",
 		description: "Dylan encounters MJ, a famous basketball player, and asks for an autograph. But Tidyping, who wants to clean everything, makes the autograph mysteriously disappear...",
@@ -34,9 +94,90 @@ const episodes = {
 		videoUrl: "?cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211259&authkey=AEzj-CN2CVYPVwE",
 		onedriveUrl: "https://31gl.short.gy/EMhDPc"
 	},
+	S2E09: {
+		title: "Yummyping Is The Best Cooking Teacher",
+		description: "After the chef quits, the business isn't going well at Eline's Seasoned Chicken Restaurant. To help her mother, Ellie practices day and night, but keeps failing over and over again. But Yummyping, who smells the chicken, decides to help her.",
+		wikiPages: ["Yummyping"],
+		specialStatus: "",
+		videoType: "OneDrive",
+		videoUrl: "cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211260&authkey=AOtT26Y8smwiCXg",
+		onedriveUrl: "https://31gl.short.gy/UCkGGD"
+	},
+	S2E10: {
+		title: "Fainted Trustping",
+		description: "While Trustping and Teeheeping are fighting each other, Trustping suddenly faints! In order to cure Trustping's disease, The Tomato Jewel is needed. With the help of Dadaping, the Teeniepings make everything possible to cure him.",
+		wikiPages: ["Tomato Jewel"],
+		specialStatus: "",
+		videoType: "OneDrive",
+		videoUrl: "?cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211263&authkey=AIcSasy_bkw44rg",
+		onedriveUrl: "https://31gl.short.gy/p7bBvD"
+	},
+	S2E11: {
+		title: "Heartsping The Knitted Doll",
+		description: "Cozyping enters in the Teenieping Academy and transforms objects into wolly creations... But when the Teeniepings wake up, a bad surprise awaits: there's a giant Heartsping doll!",
+		wikiPages: ["Cozyping"],
+		specialStatus: "",
+		videoType: "OneDrive",
+		videoUrl: "?cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211262&authkey=AMNGS5_Kg9belM8",
+		onedriveUrl: "https://31gl.short.gy/o1yNcT"
+	},
+	S2E12: {
+		title: "The Teddy Bear is Alive?",
+		description: "While everyone is relaxing, Gamyeonping takes control of Romi's teddy bear, to attack the Teenieping Academy. The Teeniepings gets afraid, and Tidyping gets devoured! All of this must end!",
+		wikiPages: ["Romi's teddy bear"],
+		specialStatus: "",
+		videoType: "OneDrive",
+		videoUrl: "?cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211261&authkey=AN2wa15-kYxSGx4",
+		onedriveUrl: "hhttps://31gl.short.gy/4wp3DA"
+	},
+	S2E13: {
+		title: "Another Princess",
+		description: "Jennie, Romi's other cousin from the Emotion Kingdom, arrives in Harmony Village. But if Romi and her are like sisters, her personal Teenieping Onlyping hates Heartsping, and an argument quickly starts...",
+		wikiPages: ["Jennie", "Onlyping"],
+		specialStatus: "",
+		videoType: "OneDrive",
+		videoUrl: "?cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211266&authkey=AFKQKpx7uUHS3xU",
+		onedriveUrl: "https://31gl.short.gy/xOQwWd"
+	},
+	S2E14: {
+		title: "Memoping's Memo Memo",
+		description: "Memoping has a really special power: eveything she notes in her notebook will become true! And when Romi finds her, a race starts, but Romi can't manage to catch Memoping...",
+		wikiPages: ["Memoping"],
+		specialStatus: "",
+		videoType: "OneDrive",
+		videoUrl: "?cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211264&authkey=AI0wI3nO1_Zja4s",
+		onedriveUrl: "https://31gl.short.gy/jXPmoE"
+	},
+	S2E15: {
+		title: "Duel! Royal Teeniepings",
+		description: "The Royal Teeniepings arrive at the Teenieping Academy, and they say that the students Teeniepings cannot help Romi properly. To keep their place, the students Teeniepings will have to win a duel...",
+		wikiPages: ["Royal Teeniepings"],
+		specialStatus: "",
+		videoType: "OneDrive",
+		videoUrl: "?cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211265&authkey=AOnyYASv-a_dXlI",
+		onedriveUrl: "https://31gl.short.gy/hI12sg"
+	},
+	S2E16: {
+		title: "Heartrose With A Big Kingdom",
+		description: "While Romi is at school, Ladyping, who fell in love with Gamyeonping, enters the bakery and creates her own kingdom, with everyone being at her service, including all the Teeniepings!",
+		wikiPages: ["Ladyping", "Gamyeonping"],
+		specialStatus: "",
+		videoType: "OneDrive",
+		videoUrl: "?cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211269&authkey=AKx2vDRS_ZMOlTk",
+		onedriveUrl: "https://31gl.short.gy/O2YCWl"
+	},
+	S2E17: {
+		title: "The Jewel Teeniepings Got Stolen!",
+		description: "Sneak peek - While everyone is sleeping, two thieves enter the bakery to steal the Jewelry Box and the Jewel Teeniepings! And when Romi wakes up, it's already too late...",
+		wikiPages: ["Thieves", "Jewelry Box"],
+		specialStatus: "",
+		videoType: "OneDrive",
+		videoUrl: "?cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211268&authkey=AI7YvfC2dLyVT64",
+		onedriveUrl: "https://31gl.short.gy/4sQhC0"
+	},
 	S2E18: {
 		title: "Who Is The Real Heartsping?",
-		description: "Buddyping is having fun duplicating everything in the bakery. And when Heartsping tries to find him, she gets duplicated into her twin... Good luck finding who's the real one!",
+		description: "Trailer - Buddyping is having fun duplicating everything in the bakery. And when Heartsping tries to find him, she gets duplicated into her twin... Good luck finding who's the real one!",
 		wikiPages: ["Buddyping"],
 		specialStatus: "Coming soon",
 		videoType: "YouTube",
@@ -44,12 +185,3 @@ const episodes = {
 		onedriveUrl: ""
 	}
 }
-
-function getParameter(name) {
-	name = name.replace(/[\[\]]/g, "\\$&");
-	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-	results = regex.exec(window.location.href);
-	if (!results || !results[2]) return null;
-	return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
-if (getParameter('episode')) {var episode = episodes[getParameter('episode')]}
