15e4677a1b768d0aa87742451e246d56ea918dcd
GuillaumeGetkoz <48527673+GuillaumeGetkoz@users.noreply.github.com>
2022-02-05 12:24:28 +0100

    Added S2E20 + link to early episodes

--- episodes.js
+++ episodes.js
@@ -1,93 +1,11 @@
 const episodes = {
-	/* S2E01: {
-		title: "Sparkling Jewel Teenieping",
-		description: "While Romi is having peaceful days in Harmony Village, Gamyeonping released all the Jewel Teeniepings to Earth. To save them, Joahping, Teeheeping and Trustping decide to go...",
-		wikiPages: ["Jewel Teeniepings", "Jewel Heart Wing Phone", "Joahping", "Teeheeping", "Trustping"],
+	"Early episodes": {
+		title: "",
+		description: "",
+		wikiPages: [],
 		specialStatus: "",
-		videoType: "YouTube",
-		videoUrl: "_Trni5YhFUw",
-		basicUrl: ""
-	},
-	S2E02: {
-		title: "Teenieping Academy Open!",
-		description: "Romi comes in contact with Dr. Monju. She will have to catch all the Jewel Teeniepings, with the 3 new students Teeniepings. And to form the newbies, Dr. Monju has prepared a little gift!",
-		wikiPages: ["Teenieping Academy", "Jewelry Box"],
-		specialStatus: "",
-		videoType: "YouTube",
-		videoUrl: "jKAXq4IJHv8",
-		basicUrl: ""
-	},
-	S2E03: {
-		title: "We Have To Put Up With the Tickling!",
-		description: "Tickleping is having fun tickling everyone of Harmony Village. Romi must catch her, but before, the Teeniepings must train to put up with the tickling!",
-		wikiPages: ["Tickleping"],
-		specialStatus: "",
-		videoType: "YouTube",
-		videoUrl: "alEFGDOSu60",
-		basicUrl: ""
-	},
-	S2E04: {
-		title: "Ayaya Stop Hurting Me",
-		description: "Trustping enjoys Ian's new chocolates so much that he eats way too many of them, and gets a bad toothache the very next day! And Owwping, who enters the bakery, makes things even worse!",
-		wikiPages: ["Owwping"],
-		specialStatus: "",
-		videoType: "YouTube",
-		videoUrl: "5Nf5Gt6ThUU",
-		basicUrl: ""
-	},
-	S2E05: {
-		title: "Wishping's Three Wishes",
-		description: "Lena, Romi's greatest enemy, discovers Wishping. To thank her, Wishping decides to grant Lena 3 wishes. But when Romi finds her, it's too late: Lena refuses to give up Wishping...",
-		wikiPages: ["Wishping"],
-		specialStatus: "",
-		videoType: "YouTube",
-		videoUrl: "WDY_SuqaN3Q",
-		basicUrl: ""
-	},
-	S2E06: {
-		title: "Pat Pat I'll Give You Warm Comfort!",
-		description: "Kyle is devastated when he learns that he failed to enter the international pastry school, and decides to leave. Joahping decides to follow him, when she suddenly runs into her friend Patping...",
-		wikiPages: ["Patping"],
-		specialStatus: "",
-		videoType: "YouTube",
-		videoUrl: "QUUXGhWLoDc",
-		basicUrl: ""
-	},
-	S2E07: {
-		title: "Disappeared Romi",
-		description: "Gamyeonping is in Harmony Village! And to make things worse, while Heartsping is searching them, Timidping turns Romi and her friends into super small persons!",
-		wikiPages: ["Timidping", "Gamyeonping"],
-		specialStatus: "",
-		videoType: "YouTube",
-		videoUrl: "ezbVkqn_Yrs",
-		basicUrl: ""
-	},
-	S2E08: {
-		title: "A Lot Of Sparkles",
-		description: "Dylan encounters MJ, a famous basketball player, and asks for an autograph. But Tidyping, who wants to clean everything, makes the autograph mysteriously disappear...",
-		wikiPages: ["Tidyping"],
-		specialStatus: "",
-		videoType: "OneDrive",
-		videoUrl: "?cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211259&authkey=AEzj-CN2CVYPVwE",
-		basicUrl: "https://31gl.short.gy/EMhDPc"
-	},
-	S2E09: {
-		title: "Yummyping Is The Best Cooking Teacher",
-		description: "After the chef quits, the business isn't going well at Eline's Seasoned Chicken Restaurant. To help her mother, Ellie practices day and night, but keeps failing over and over again. But Yummyping, who smells the chicken, decides to help her.",
-		wikiPages: ["Yummyping"],
-		specialStatus: "",
-		videoType: "OneDrive",
-		videoUrl: "?cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211260&authkey=AOtT26Y8smwiCXg",
-		basicUrl: "https://31gl.short.gy/UCkGGD"
-	}, */
-	S2E10: {
-		title: "Fainted Trustping",
-		description: "While Trustping and Teeheeping are fighting each other, Trustping suddenly faints! In order to cure Trustping's disease, The Tomato Jewel is needed. With the help of Dadaping, the Teeniepings make everything possible to cure him.",
-		wikiPages: ["Tomato Jewel", "Trustping"],
-		specialStatus: "",
-		videoType: "OneDrive",
-		videoUrl: "?cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211263&authkey=AJILwMu5SXh__88",
-		basicUrl: "https://1drv.ms/v/s!AiHQU8LjSx_xiW8rEJLdjmNX4LTj"
+		videoType: "redirect",
+		videoUrl: "https://catchteenieping.fandom.com/wiki/Catch!_Teenieping#List_of_episodes"
 	},
 	S2E11: {
 		title: "Heartsping The Knitted Doll",
@@ -172,12 +90,11 @@ const episodes = {
 	},
 	S2E20: {
 		title: "I'm Going To Sculpt!",
-		description: "Trailer - Sculptping tries to sculpt a Teenieping of the Jewel Forest, but he can't remember her. Later, his statue is found by the Teeniepings, who take it at the Teenieping Academy. But suddenly, the statue starts to move!",
+		description: "Sculptping tries to sculpt a Teenieping of the Jewel Forest, but he can't remember her. Later, his statue is found by the Teeniepings, who take it at the Teenieping Academy. But suddenly, the statue starts to move!",
 		wikiPages: ["Sculptping"],
-		specialStatus: "Coming soon",
-		videoType: "YouTube",
-		videoUrl: "ypBmMNNT9nM",
-		basicUrl: ""
+		specialStatus: "",
+		videoType: "Google Drive",
+		videoUrl: "1xPKeJthwBGoqqvcAl_Uy9c11Y3V_Y4h0"
 	},
 	S2E21: {
 		title: "Dangerous Picnic",
@@ -185,7 +102,6 @@ const episodes = {
 		wikiPages: ["Jennie", "Gamyeonping"],
 		specialStatus: "Coming soon",
 		videoType: "YouTube",
-		videoUrl: "4SwytJIkgLg",
-		basicUrl: ""
+		videoUrl: "4SwytJIkgLg"
 	}
 }
