6c1891a195bd15e951a6033dc3629584f97aa2c3
GuillaumeGetkoz <48527673+GuillaumeGetkoz@users.noreply.github.com>
2022-03-19 08:54:17 +0100

    Changed various things in the UI

--- episodes.js
+++ episodes.js
@@ -1,98 +1,95 @@
 const episodes = {
 	"Early episodes": {
-		title: "",
-		description: "",
-		wikiPages: [],
-		specialStatus: "",
+		status: "Early episodes",
 		videoType: "redirect",
 		videoUrl: "https://catchteenieping.fandom.com/wiki/Twinkle_Catch!_Teenieping#Episodes"
 	},
 	S2E17: {
+		number: 17,
 		title: "The Jewel Teeniepings Got Stolen!",
 		description: "While everyone is sleeping, two thieves enter the bakery to steal the Jewelry Box and the Jewel Teeniepings! And when Romi wakes up, it's already too late...",
 		wikiPages: ["Thieves", "Jewelry Box"],
-		specialStatus: "",
 		videoType: "OneDrive",
 		videoUrl: "?cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211271&authkey=ALAyX7GbKRBpqDI",
 		basicUrl: "https://1drv.ms/v/s!AiHQU8LjSx_xiXeVpFuC20Oba4PO"
 	},
 	S2E18: {
+		number: 18,
 		title: "Who Is The Real Heartsping?",
 		description: "Buddyping is having fun duplicating everything in the bakery. And when Heartsping tries to find him, she gets duplicated into her twin... Good luck finding who's the real one!",
 		wikiPages: ["Buddyping", "Heartsping"],
-		specialStatus: "",
 		videoType: "OneDrive",
 		videoUrl: "?cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211270&authkey=AIScG5Mx2CN4CUU",
 		basicUrl: "https://1drv.ms/v/s!AiHQU8LjSx_xiXYPLs2AwbD4lomU"
 	},
 	S2E19: {
+		number: 19,
 		title: "Find The Gift Box!",
 		description: "Giftping likes to give gifts to everyone! But when she gets chased by Romi, instead of taking her prop, she takes another gift and runs... She will have to recover her magic prop, but how?",
 		wikiPages: ["Giftping", "Luca"],
-		specialStatus: "",
 		videoType: "OneDrive",
 		videoUrl: "?cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211272&authkey=APNKoue7wbpy85k",
 		basicUrl: "https://1drv.ms/v/s!AiHQU8LjSx_xiXhf3m90OnPnIqtt"
 	},
 	S2E20: {
+		number: 20,
 		title: "I'm Going To Sculpt!",
 		description: "Sculptping tries to sculpt a Teenieping of the Jewel Forest, but he can't remember her. Later, his statue is found by the Teeniepings, who take it at the Teenieping Academy. But suddenly, the statue starts to move!",
 		wikiPages: ["Sculptping"],
-		specialStatus: "",
 		videoType: "OneDrive",
 		videoUrl: "?cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211273&authkey=AO354LpNvCRc044",
 		basicUrl: "https://1drv.ms/v/s!AiHQU8LjSx_xiXmZpBHVdnFbq-cY"
 	},
 	S2E21: {
+		number: 21,
 		title: "Dangerous Picnic",
 		description: "Jennie invites everyone to go to a picnic, and Heartsping, who has a bad feeling about this, is forced to come. But while everyone is having fun, Gamyeonping suddenly appears!",
 		wikiPages: ["Jennie", "Gamyeonping"],
-		specialStatus: "",
 		videoType: "OneDrive",
 		videoUrl: "?cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211274&authkey=AERrugMJABqpVLk",
 		basicUrl: "https://1drv.ms/v/s!AiHQU8LjSx_xiXoP-QmI7tSIESTP"
 	},
 	S2E22: {
+		number: 22,
 		title: "Pinker Teenieping Ballet",
 		description: "When Princess Ruby approaches Balletping, the latter reveals her secret: she wants to have a friend to make a ballet with them. But then, she hears about the Teenieping Ballet...",
 		wikiPages: ["Balletping"],
-		specialStatus: "",
 		videoType: "OneDrive",
 		videoUrl: "?cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211275&authkey=AOxtq6RrjplHegw",
 		basicUrl: "https://1drv.ms/v/s!AiHQU8LjSx_xiXt2fw4oj_rgAmlS"
 	},
 	S2E23: {
+		number: 23,
 		title: "Hero Teenieping, Wonderping!",
 		description: "Wonderping, a self-proclaimed heroine, is always here to help others! Everyone is looking for her, but when she appears, she gets Princess Ruby into a lot of trouble!",
 		wikiPages: ["Wonderping"],
-		specialStatus: "",
 		videoType: "OneDrive",
 		videoUrl: "?cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211276&authkey=AKkGJvCGAWmTFk0",
 		basicUrl: "https://1drv.ms/v/s!AiHQU8LjSx_xiXytBJZriGYIRxCS"
 	},
 	S2E24: {
+		number: 24,
 		title: "Teenieping Academy Graduation Ceremony",
 		description: "Since all the Jewel Teeniepings are now caught, a graduation ceremony is organized at the Teenieping Academy. Everyone joins, even Jennie and the Royal Teeniepings! Let the party begin!",
 		wikiPages: ["Teenieping Academy", "Jewel Teeniepings"],
-		specialStatus: "",
 		videoType: "OneDrive",
 		videoUrl: "?cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211277&authkey=AACvdgtA-Jlwnwo",
 		basicUrl: "https://1drv.ms/v/s!AiHQU8LjSx_xiX26qx0pSOIGUyQj"
 	},
 	S2E25: {
+		number: 25,
 		title: "Heartsping In Danger",
 		description: "Heartsping has been made prisoner by Gamyeonping! To save them, Romi, helped by Jennie, go to the Emotion Kingdom and they promise to save Heartsping. Meanwhile, the latter discovers in her hiding place something very strange...",
 		wikiPages: ["Gamyeonping", "Jennie", "Emotion Kingdom"],
-		specialStatus: "",
 		videoType: "OneDrive",
 		videoUrl: "?cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211278&authkey=ANxOTfp52nuDHWc",
 		basicUrl: "https://1drv.ms/v/s!AiHQU8LjSx_xiX4muCdR2mY6EaNL"
 	},
 	S2E26: {
+		number: 26,
 		title: "Return Of Jewel Forest",
 		description: "After defeating Gamyeonping, Romi finally discovers the whole truth... The story of her friend who stayed in the shadow for way too long... But the battle is not over yet.",
-		wikiPages: ["Jennie", "Onlyping", "Gamyeonping", "Jewel Forest", "Emotion Kingdom"],
-		specialStatus: "",
+		wikiPages: ["Jennie", "Emotion Kingdom"],
 		videoType: "OneDrive",
 		videoUrl: "?cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211280&authkey=AFs-hRQIyD-AbBc",
 		basicUrl: "https://1drv.ms/v/s!AiHQU8LjSx_xigDdKqGvBxhPJZRc"
