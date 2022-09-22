a1af120df65696d9a06fb5782011ef1b98b2efea
Glowsham <shundryft@gmail.com>
2022-09-22 19:03:11 +0200

    Site opened for S3 😄

--- episodes.js
+++ episodes.js
@@ -1,27 +1,22 @@
 const episodes = {
-	"Early episodes": {
-		status: "Early episodes",
-		videoType: "redirect",
-		videoUrl: "https://catchteenieping.fandom.com/wiki/Twinkle_Catch!_Teenieping#Episodes"
+	S3E01: {
+		number: 1,
+		title: "Secret Key Teenieping",
+		description: "Jennie strikes once again, and releases all the Key Teeniepings to Earth! Meanwhile, Okeydokeyping, Nanaping, and Trueping, who watched the whole scene, jump into a portal to join them, and fall on Romi herself!",
+		wikiPages: ["Key Teeniepings", "Mystic Village"],
+		videoType: "Dailymotion",
+		videoUrl: "x8dv2j6",
+		//discussion: "nf",
+		onedrive: "https://1drv.ms/v/s!AiHQU8LjSx_xigeHv90T5pa0NoHH"
 	},
-	S2E25: {
-		number: 25,
-		title: "Heartsping In Danger",
-		description: "Heartsping has been made prisoner by Gamyeonping! To save them, Romi, helped by Jennie, go to the Emotion Kingdom and they promise to save Heartsping. Meanwhile, the latter discovers in her hiding place something very strange...",
-		wikiPages: ["Gamyeonping", "Jennie", "Emotion Kingdom"],
-		videoType: "OneDrive",
-		videoUrl: "?cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211278&authkey=ANxOTfp52nuDHWc",
-		basicUrl: "https://1drv.ms/v/s!AiHQU8LjSx_xiX4muCdR2mY6EaNL",
-		alternative: "https://uptostream.com/iframe/b27spzpg8gd9"
+	S3E02: {
+		number: 2,
+		title: "Transformation! Floraheart",
+		description: "When Romi returns to the bakery, Jun is in a super energetic state of mind, all because of Quickping! Romi and the Teeniepings quickly get sight of the intruder and a race starts, but Romi gets hit by Quickping's magic!",
+		wikiPages: ["Quickping"],
+		videoType: "Dailymotion",
+		videoUrl: "x8dv2mz",
+		onedrive: "https://1drv.ms/v/s!AiHQU8LjSx_xigggF0VAqcKGmksz"
 	},
-	S2E26: {
-		number: 26,
-		title: "Return Of Jewel Forest",
-		description: "After defeating Gamyeonping, Romi finally discovers the whole truth... The story of her friend who stayed in the shadow for way too long... But the battle is not over yet.",
-		wikiPages: ["Jennie", "Emotion Kingdom"],
-		videoType: "OneDrive",
-		videoUrl: "?cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211280&authkey=AFs-hRQIyD-AbBc",
-		basicUrl: "https://1drv.ms/v/s!AiHQU8LjSx_xigDdKqGvBxhPJZRc",
-		alternative: "https://uptostream.com/iframe/t0swvjwhfcby"
-	}
+	S3E03: {}
 }
