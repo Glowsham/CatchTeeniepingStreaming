965e855e9a445d33a6ab74b228ad627fc341e94d
Glowsham <shundryft@gmail.com>
2023-02-19 13:13:50 +0100

    Added rest of Bilibili links

--- episodes.js
+++ episodes.js
@@ -5,7 +5,8 @@ const episodes = {
 		description: "Jennie strikes once again, and releases all the Key Teeniepings to Earth! Meanwhile, Okeydokeyping, Nanaping, and Trueping, who watched the whole scene, jump into a portal to join them, and fall on Romi herself!",
 		wikiPages: ["Key Teeniepings", "Mystic Village"],
 		videoType: "Dailymotion",
-		videoUrl: "x8gpnld",
+		videoUrl: "x8iflbi",
+		bilibili: "2042625987",
 		onedrive: "https://1drv.ms/v/s!AiHQU8LjSx_xihpOXmCdj9-LPjo0?e=7Wp15d"
 	},
 	S3E02: {
@@ -14,7 +15,8 @@ const episodes = {
 		description: "When Romi returns to the bakery, Jun is in a super energetic state of mind, all because of Quickping! Romi and the Teeniepings quickly get sight of the intruder and a race starts, but Romi gets hit by Quickping's magic!",
 		wikiPages: ["Quickping"],
 		videoType: "Dailymotion",
-		videoUrl: "x8gpnle",
+		videoUrl: "x8iflbr",
+		bilibili: "2044419442",
 		onedrive: "https://1drv.ms/v/s!AiHQU8LjSx_xihtL5U50a5zur17t?e=GQ2j7M"
 	},
 	S3E03: {
@@ -23,7 +25,8 @@ const episodes = {
 		description: "Romi and the Teeniepings are helping at Raymond's snack bar, but food keeps disappearing: because of Yamyamping, someone is eating all the food in secret! Will the princess be able to put an end to this mess?",
 		wikiPages: ["Yamyamping"],
 		videoType: "Dailymotion",
-		videoUrl: "x8gpnlf",
+		videoUrl: "x8iflc5",
+		bilibili: "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
 		onedrive: "https://1drv.ms/v/s!AiHQU8LjSx_xihleDcOD-T-CtmL7?e=lt2N6Y"
 	},
 	S3E04: {
@@ -31,8 +34,8 @@ const episodes = {
 		title: "Hot Hot Camping Commotion",
 		description: "Everyone is excited about today's camping party... except Trueping who would have rather gone to the beach. But while he is taking care of the fire, it gets repeatedly extinguished! Is Trueping having illusions, or is someone behind all this?",
 		wikiPages: ["Hotping"],
-		videoType: "Dailymotion",
-		videoUrl: "x8gpnli",
+		videoType: "Bilibili",
+		videoUrl: "4786175240441344",
 		onedrive: "https://1drv.ms/v/s!AiHQU8LjSx_xihNT3uV46qGn8llo?e=3d29ya"
 	},
 	S3E05: {
@@ -40,8 +43,8 @@ const episodes = {
 		title: "I'll Treat You, Beep",
 		description: "Beeping is here to take care of the people of Harmony Town... except she has a radical method to heal them! When Lena gets coated with a plaster cast, Romi and the Teeniepings know it's time to catch the troublemaker!",
 		wikiPages: ["Beeping", "Nursepings"],
-		videoType: "Dailymotion",
-		videoUrl: "x8gqzf3",
+		videoType: "Bilibili",
+		videoUrl: "4786175224581120",
 		onedrive: "https://1drv.ms/v/s!AiHQU8LjSx_xihFN_PoFm63cvVqZ?e=fOGyEk"
 	},
 	S3E06: {
@@ -49,8 +52,8 @@ const episodes = {
 		title: "Teenieping Hospital is Here",
 		description: "As Beeping just opened the new Teenieping Hospital, which didn't have as much success as planned, Nanaping suddenly faints and is taken by force by Beeping and her team! What in the world is going on?",
 		wikiPages: ["Beeping", "Nanaping"],
-		videoType: "Dailymotion",
-		videoUrl: "x8gpnli",
+		videoType: "Bilibili",
+		videoUrl: "2048815680",
 		onedrive: "https://1drv.ms/v/s!AiHQU8LjSx_xihLtviRKhPz18jJM?e=wpHnjc"
 	},
 	S3E07: {
@@ -58,8 +61,8 @@ const episodes = {
 		title: "Cheer Cheer I’m a Cheerleader",
 		description: "Cheerping is here to help the people of Harmony Town! As Okeydokeyping can't manage to jump on the vaulting boxes, Cheerping makes a striking entrance into the Secret Castle to cheer her up!",
 		wikiPages: ["Cheerping"],
-		videoType: "Dailymotion",
-		videoUrl: "x8gqzf4",
+		videoType: "Bilibili",
+		videoUrl: "4786175228644352",
 		onedrive: "https://1drv.ms/v/s!AiHQU8LjSx_xihXjFuHQfszYvnql?e=gNcWky"
 	},
 	S3E08:{
@@ -67,8 +70,8 @@ const episodes = {
 		title: "Fix It, Fixping",
 		description: "The Teeniepings ignore Sarah's order and have fun with the robot vacuum, until it suddenly breaks! But luckily, Fixping arrives and uses his powers to fix the robot... But then, things get worse, as the robot became evil!",
 		wikiPages: ["Fixping"],
-		videoType: "Dailymotion",
-		videoUrl: "x8h7gxn",
+		videoType: "Bilibili",
+		videoUrl: "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!",
 		onedrive: "https://1drv.ms/v/s!AiHQU8LjSx_xihRG_pDpUNaJlBaZ?e=YYHiuE"
 	},
 	S3E09: {
@@ -76,8 +79,8 @@ const episodes = {
 		title: "Romi and Heartsping Have Changed",
 		description: "What a terrible fate: because of Enchantping, Romi and Heartsping have switched bodies! How will the girls make everything return to normal, and how will Romi catch Enchantping in those conditions? A crazy adventure awaits...",
 		wikiPages: ["Enchantping"],
-		videoType: "Dailymotion",
-		videoUrl: "x8h7cmr",
+		videoType: "Bilibili",
+		videoUrl: "2048763126",
 		onedrive: "https://1drv.ms/v/s!AiHQU8LjSx_xig8XRHreSmoxyy8y?e=2aJb7L"
 	},
 	S3E10: {
@@ -85,8 +88,8 @@ const episodes = {
 		title: "Snow White Romi",
 		description: "Romi and the Teeniepings have disappeared! Well, to be exact, they all get transported into Snow White's universe... the latter embodied by Romi in person! The Royal Teeniepings try to save them, but they make things even worse...",
 		wikiPages: ["Magical fairytale book"],
-		videoType: "Dailymotion",
-		videoUrl: "x8h7bmu",
+		videoType: "Bilibili",
+		videoUrl: "2045259947",
 		onedrive: "https://1drv.ms/v/s!AiHQU8LjSx_xihDwvDjK9J7LcJp9?e=3BTmcF"
 	},
 	S3E11: {
@@ -94,8 +97,8 @@ const episodes = {
 		title: "Fashionping is Making Clothes",
 		description: "Fashionping is giving very strange makeovers to Lena... But the fashion artist doesn't seem satisfied, so she looks for some more inspiration when she gets face to face with Princess Solaris... To whom she gives another makeover!",
 		wikiPages: ["Fashionping"],
-		videoType: "Dailymotion",
-		videoUrl: "x8h4t6p",
+		videoType: "Bilibili",
+		videoUrl: "4786175152491520",
 		onedrive: "https://1drv.ms/v/s!AiHQU8LjSx_xig4jAtM1wnTMMz0f?e=MPm7L0"
 	},
 	S3E12: {
@@ -103,8 +106,8 @@ const episodes = {
 		title: "Don't Move, Policeping",
 		description: "Romi leaves her precious rings to the Teeniepings, but an unpleasant surprise awaits on her return: the rings have been stolen! But as they call for the police, Policeping enters the bakery: will he team up with Romi to find the missing rings?",
 		wikiPages: ["Policeping"],
-		videoType: "Dailymotion",
-		videoUrl: "x8h4n19",
+		videoType: "Bilibili",
+		videoUrl: "4786175153409536",
 		onedrive: "https://1drv.ms/v/s!AiHQU8LjSx_xigykwE6zOVd97mqm?e=nmuNH8"
 	},
 	S3E13: {
@@ -112,8 +115,8 @@ const episodes = {
 		title: "Sweets Full of Luck",
 		description: "Jun shows his new fortune cookies to the Teeniepings, which reminds them of their time in the Mystic Village: by eating a magic fortune cookie, their luck is revealed. Romi then decides to surprise everyone, by organizing the Luck Festival!",
 		wikiPages: ["Key Teeniepings", "Mystic Village", "Luckyping"],
-		videoType: "Dailymotion",
-		videoUrl: "x8h02wd",
+		videoType: "Bilibili",
+		videoUrl: "2047031296",
 		onedrive: "https://1drv.ms/v/s!AiHQU8LjSx_xig3TgyiVGvnWuGdI?e=p6Iy3C"
 	},
 	S3E14: {
@@ -121,8 +124,8 @@ const episodes = {
 		title: "Splash Splash, Splashping",
 		description: "Splashping, the mermaid Teenieping, is swimming in the sea with a pink dolphin, when the latter gets imprisoned in a fishing net. Splashping rushes to save her friend, and she ends up in Harmony Town's aquarium!",
 		wikiPages: ["Splashping"],
-		videoType: "OneDrive",
-		videoUrl: "?cid=F11F4BE3C253D021&resid=F11F4BE3C253D021%211290&authkey=AOYJzToJbtPRFg0",
+		videoType: "Dailymotion",
+		videoUrl: "2049319418",
 		basicUrl: "https://1drv.ms/v/s!AiHQU8LjSx_xigpyFbRkpNo_r3jf?e=XPidAQ"
 	},
 	S3E15: {
@@ -130,8 +133,8 @@ const episodes = {
 		title: "Partyping and Party Party",
 		description: "Romi and Marylou are organizing a huge birthday party for Marylou in the Heartrose Bakery... when Partyping comes around and ruins all the preparatives! Will the girls manage to prepare everything on time, or will Marylou's birthday be a fail?",
 		wikiPages: ["Partyping", "Marylou"],
-		videoType: "Dailymotion",
-		videoUrl: "x8h4lbh",
+		videoType: "Bilibili",
+		videoUrl: "4786168690904064",
 		onedrive: "https://1drv.ms/v/s!AiHQU8LjSx_xihRG_pDpUNaJlBaZ?e=ag67I0"
 	},
 	S3E16: {
@@ -139,8 +142,8 @@ const episodes = {
 		title: "Make a Cool Hairdo, Decorping",
 		description: "How awful! Romi is having a terrible hair day... On the Teeniepings' advice, she goes to Harmony Town's hairdressing salon, where she comes face to face with Decorationping. She gets ready to catch her, but the hairdresser won't let this happen!",
 		wikiPages: ["Decorping", "Bibi"],
-		videoType: "Dailymotion",
-		videoUrl: "x8gpkah",
+		videoType: "Bilibili",
+		videoUrl: "2044974661",
 		onedrive: "https://1drv.ms/v/s!AiHQU8LjSx_xihZ-Hs_jYvft_yG1?e=7epMK4"
 	},
 	S3E17: {
