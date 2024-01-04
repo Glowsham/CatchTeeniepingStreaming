481077f61dd84554abbff098b3f363f3383bfd1e
Glowsham <shundryft@gmail.com>
2024-01-04 13:42:35 +0100

    added ep14 + ep15 + other various changes

--- episodesList.js
+++ episodesList.js
@@ -3,7 +3,7 @@ const episodes = {
 		S4E01: {
 			number: 1,
 			title: "Sweet and Sour Dessert Teenieping",
-			description: "The desserts in the Dessert Village all appear to taste very strange! Moreover, the Magic Sugar Ball that protects the village has disappeared, and so have the Dessert Teeniepings! What on Earth is going on!?",
+			description: "Something strange is happening with the desserts of the Dessert Village: the tasty treats taste unusually odd! To make matters worse, the protective Magic Sugar Ball and the adorable Dessert Teeniepings have vanished without a trace. What could possibly be the explanation for this  mystery?",
 			videoUrl: "https://geo.dailymotion.com/player/xjrv1.html?video=x8of1sk",
 			videoAlts: {
 				"Bilibili": "https://www.bilibili.tv/en/video/4788702030730752"
@@ -48,7 +48,7 @@ const episodes = {
 		S4E06: {
 			number: 6,
 			title: "I Want To be a Royal Teenieping",
-			description: "When Donutping sees Romi transform, she only wants one thing: to transform with her! Unfortunately, this is something only the Royal Teeniepings can do... But Donutping has an idea in mind!",
+			description: "When Donutping sees Romi transform, she only wants one thing: to transform with her! Unfortunately, this is something only the Royal Teeniepings can do this... But Donutping has an idea in mind!",
 			videoUrl: "https://geo.dailymotion.com/player/xjrv1.html?video=x8p48wa",
 			videoAlts: {
 				"Bilibili": "https://www.bilibili.tv/en/video/4788997083765248"
@@ -75,7 +75,7 @@ const episodes = {
 		S4E09: {
 			number: 9,
 			title: "Fluffyping And Her Tiny Friends",
-			description: "During a fight against Puffping, a strange phenomenon happens, which results in Princess Berryheart and the Royal Teeniepings - with the exception of Fluffyping - becoming giant! And in order to save her tiny friends, Fluffyping has no choice but to fight on her own...",
+			description: "During a fight against Puffping, a strange phenomenon happens, which results in Princess Berryheart and the Royal Teeniepings - with the exception of Fluffyping - becoming tiny! And in order to save her tiny friends, Fluffyping has to find Puffping...",
 			videoUrl: "https://geo.dailymotion.com/player/xjrv1.html?video=x8possd",
 			videoAlts: {
 				"Bilibili": "https://www.bilibili.tv/en/video/4789255691043328"
@@ -111,204 +111,201 @@ const episodes = {
 		S4E13: {
 			number: 13,
 			title: "Romi Goes To The Dessert Village",
-			description: "Romi's Sugar Berry Pact starts to behave in an unusual way... so unusual, actually, that it creates a portal transporting Romi and the Teeniepings to the Dessert Village, in ruins because of the lack of Teeniepings...",
-			//videoType: "trailer",
+			description: "Romi's Sugar Berry Pact starts to behave unusually... so unusual that it creates a portal transporting Romi and the Teeniepings to the Dessert Village, in ruins because of the lack of Teeniepings!",
 			videoUrl: "https://geo.dailymotion.com/player/xjrv1.html?video=x8qlvlh",
 			videoAlts: {
 				"Bilibili": "https://www.bilibili.tv/en/video/4789609907749376"
 			}
+		},
+		S4E14: {
+			number: 14,
+			title: "You Can't Cry! Cupping, Muffping",
+			description: "Teeniepings may be cute, but baby Teeniepings like Cupping and Muffping are even cuter! But not when it comes to catching them, as the two siblings know how to avoid their enemies, and worse, they become giants when they cry!",
+			videoUrl: "https://geo.dailymotion.com/player/xjrv1.html?video=x8r5hz1",
+			videoAlts: {
+				"Bilibili": "https://www.bilibili.tv/en/video/4789788622848512"
+			}
+		},
+		S4E15: {
+			number: 15,
+			title: "Taking Care Of A Baby Is Hard, Again",
+			description: "Cupping and Muffping are the stars of the Teenieping Village! Everyone loves them, even when they create a mess at the restaurant... Except for Donutping, who is not only outshined by the two but is also given the difficult task of babysitting them!",
+			videoType: "trailer",
+			videoUrl: "https://geo.dailymotion.com/player/xjrv1.html?video=x8r5hrb"
 		}
 	},
 
-	"<b>NEW!</b> Season 2": {
+	"Season 2": {
 		S2E01: {
 			number: 1,
 			title: "Twinkle Twinkle Jewel Teeniepings",
 			description: "While Romi is having peaceful days in Harmony Town, Maskping, a mysterious Teenieping, releases all the Jewel Teeniepings of the Jewel Forest on Earth. To save them, three students of the Teenieping School decide to secretly go with the Jewel Heart Wing Phone.",
-			videoUrl: "https://drive.google.com/file/d/1rFbivaThQzIUK8ippFpnafZnIbFb4idL/preview",
-			message: "NetflixWatch"
+			videoUrl: "https://drive.google.com/file/d/1rFbivaThQzIUK8ippFpnafZnIbFb4idL/preview"
 		},
 		S2E02: {
 			number: 2,
 			title: "Welcome to Teenieping School!",
 			description: "After Romi finally got the Jewel Heart Wing Phone, she comes in contact with Dr. Monziu. The latter decides that the students Teeniepings will help Romi that will once again be in charge of catching all the Teeniepings. Later, Romi receives he Teenieping School, a new house for the Teeniepings.",
-			videoUrl: "https://drive.google.com/file/d/1jWHpZllqy-mlCE03itfBVy8lB0Cla95J/preview",
-			message: "NetflixWatch"
+			videoUrl: "https://drive.google.com/file/d/1jWHpZllqy-mlCE03itfBVy8lB0Cla95J/preview"
 		},
 		S2E03: {
 			number: 3,
 			title: "Tickle Tickle, Try Not to Laugh!",
 			description: "While the Teeniepings discover Romi's school, Tickleping is having fun tickling everyone in Harmony Town. And, to catch her, the Teeniepings will have to train to put up with the tickling, and the winner will have to fight Tickleping.",
-			videoUrl: "https://drive.google.com/file/d/1iEHe-XRM2fSwm3dJcOmH97avNEcfLmcW/preview",
-			message: "NetflixWatch"
+			videoUrl: "https://drive.google.com/file/d/1iEHe-XRM2fSwm3dJcOmH97avNEcfLmcW/preview"
 		},
 		S2E04: {
 			number: 4,
 			title: "Ow Ow, No More Boo-Boos!",
 			description: "Trustping enjoys Ian's new chocolates so much that he eats way too many of them, and gets a bad toothache the very next day! And Owwping, who enters the bakery, makes things even worse!",
-			videoUrl: "https://drive.google.com/file/d/1vkOCC_XTWDCtE9DFQM4MhxdDGfe4Uli0/preview",
-			message: "NetflixWatch"
+			videoUrl: "https://drive.google.com/file/d/1vkOCC_XTWDCtE9DFQM4MhxdDGfe4Uli0/preview"
 		},
 		S2E05: {
 			number: 5,
 			title: "Wishping's Three Wishes",
 			description: "Lena, Romi's greatest enemy, discovers Wishping. To thank her, Wishping decides to grant Lena 3 wishes. But when Romi finds her, it's too late: Lena refuses to give up Wishping...",
-			videoUrl: "https://drive.google.com/file/d/15eNBQUrF0wl6talRh0OfOz2KWpxPNCOs/preview",
-			message: "NetflixWatch"
+			videoUrl: "https://drive.google.com/file/d/15eNBQUrF0wl6talRh0OfOz2KWpxPNCOs/preview"
 		},
 		S2E06: {
 			number: 6,
 			title: "Pat Pat, It's Okay",
 			description: "Kyle is devastated when he learns that he failed to enter the international pastry school, and decides to leave. Joahping decides to follow him, when she suddenly runs into her friend Patping...",
-			videoUrl: "https://drive.google.com/file/d/1dX8TVzJBK5PQMglLXcZChoNDrgHqM4sY/preview",
-			message: "NetflixWatch"
+			videoUrl: "https://drive.google.com/file/d/1dX8TVzJBK5PQMglLXcZChoNDrgHqM4sY/preview"
 		},
 		S2E07: {
 			number: 7,
 			title: "Shrinking Romi",
 			description: "Maskping is in Harmony Town! And to make things worse, while Heartsping is searching for them, Timidping turns Romi and her friends into super small persons!",
-			videoUrl: "https://drive.google.com/file/d/1yXxh05DIFSh_LJnkgkYDO7UT4yF5UD-O/preview",
-			message: "NetflixWatch"
+			videoUrl: "https://drive.google.com/file/d/1yXxh05DIFSh_LJnkgkYDO7UT4yF5UD-O/preview"
 		},
 		S2E08: {
 			number: 8,
 			title: "Tidy Up Until It Sparkles!",
 			description: "Dylan encounters MJ, a famous basketball player, and asks for an autograph. But Tidyping, who wants to clean everything, makes the autograph mysteriously disappear...",
-			videoUrl: "https://drive.google.com/file/d/19sy_PjQWNugwWbmFj6rAM8hw26Ef10jh/preview",
-			message: "NetflixWatch"
+			videoUrl: "https://drive.google.com/file/d/19sy_PjQWNugwWbmFj6rAM8hw26Ef10jh/preview"
 		},
 		S2E09: {
 			number: 9,
 			title: "Top Chef Yummyping",
 			description: "After the chef quits, the business isn't going well at Eline's Seasoned Chicken Restaurant. To help her mother, Ellie practices day and night, but keeps failing over and over again. But Yummyping, who smells the chicken, decides to help her.",
-			videoUrl: "https://drive.google.com/file/d/1-oA_nMKlD99Az3dGSAsqtqyE_ds0whCo/preview",
-			message: "NetflixWatch"
+			videoUrl: "https://drive.google.com/file/d/1-oA_nMKlD99Az3dGSAsqtqyE_ds0whCo/preview"
 		},
 		S2E10: {
 			number: 10,
 			title: "Trustping Has Fainted!",
 			description: "While Trustping and Teeheeping are fighting each other, Trustping suddenly faints! In order to cure Trustping's disease, The Tomato Jewel is needed. With the help of Dadaping, the Teeniepings make everything possible to cure him.",
-			videoUrl: "https://drive.google.com/file/d/1PYU-huHKLACAaV3Go9ejnrIrtWICAwM_/preview",
-			message: "NetflixWatch"
+			videoUrl: "https://drive.google.com/file/d/1PYU-huHKLACAaV3Go9ejnrIrtWICAwM_/preview"
 		},
 		S2E11: {
 			number: 11,
 			title: "Things Get Wooly!",
 			description: "Cozyping enters the Teenieping School and transforms objects into wooly creations... But when the Teeniepings wake up, a bad surprise awaits: there's a giant Heartsping doll!",
-			videoUrl: "https://drive.google.com/file/d/1m7b1QovUtFznb_-ycwRXr_XBuqYLveHc/preview",
-			message: "NetflixWatch"
+			videoUrl: "https://drive.google.com/file/d/1m7b1QovUtFznb_-ycwRXr_XBuqYLveHc/preview"
 		},
 		S2E12: {
 			number: 12,
 			title: "A Teddy Bear Comes to Life",
 			description: "While everyone is relaxing, Maskping takes control of Romi's teddy bear, to attack the Teenieping School. The Teeniepings get afraid, and Tidyping gets devoured! All of this must end!",
-			videoUrl: "https://drive.google.com/file/d/1w-ifrmwscntqPIKLqcz3HOcuI6krOidS/preview",
-			message: "NetflixWatch"
+			videoUrl: "https://drive.google.com/file/d/1w-ifrmwscntqPIKLqcz3HOcuI6krOidS/preview"
 		},
 		S2E13: {
 			number: 13,
 			title: "Another Princess",
 			description: "Jennie, Romi's other cousin from the Emotions Kingdom, arrives in Harmony Town. But if Romi and she are like sisters, her personal Teenieping Onlyping hates Heartsping, and an argument quickly starts...",
-			videoUrl: "https://drive.google.com/file/d/1Q0uuSOwvjnGEdJkbHNxlga4elVDHQKT_/preview",
-			message: "NetflixWatch"
+			videoUrl: "https://drive.google.com/file/d/1Q0uuSOwvjnGEdJkbHNxlga4elVDHQKT_/preview"
 		},
 		S2E14: {
 			number: 14,
 			title: "Memo This, Memoping!",
 			description: "Memoping has a really special power: everything she notes in her notebook will become true! And when Romi finds her, a race starts, but Romi can't manage to catch Memoping...",
-			videoUrl: "https://drive.google.com/file/d/1H0B7NjA22ZC44X-jtee8ZcQNP3qd5Fjg/preview",
-			message: "NetflixWatch"
+			videoUrl: "https://drive.google.com/file/d/1H0B7NjA22ZC44X-jtee8ZcQNP3qd5Fjg/preview"
 		},
 		S2E15: {
 			number: 15,
 			title: "Royal Teeniepings' Face-Off!",
 			description: "The Royal Teeniepings arrive at the Teenieping School, and they say that the students Teeniepings cannot help Romi properly. To keep their place, the students Teeniepings will have to win a duel...",
-			videoUrl: "https://drive.google.com/file/d/1C5tKKKzC8xN4NTPW1MfDq6s-Vgtso03H/preview",
-			message: "NetflixWatch"
+			videoUrl: "https://drive.google.com/file/d/1C5tKKKzC8xN4NTPW1MfDq6s-Vgtso03H/preview"
 		},
 		S2E16: {
 			number: 16,
 			title: "Heartrose Becomes a Kingdom",
 			description: "While Romi is at school, Ladyping, who was tricked by Maskping, enters the bakery and creates her own kingdom, with everyone being at her service, including all the Teeniepings!",
-			videoUrl: "https://drive.google.com/file/d/1ZB7aNQiS-sEgptvd-mJxqAujZ4LuP4PY/preview",
-			message: "NetflixWatch"
+			videoUrl: "https://drive.google.com/file/d/1ZB7aNQiS-sEgptvd-mJxqAujZ4LuP4PY/preview"
 		},
 		S2E17: {
 			number: 17,
 			title: "A Jewel Teenieping Heist!",
 			description: "While everyone is sleeping, two Thieves|thieves enter the bakery to steal the Jewelry Box and the Jewel Teeniepings! And when Romi wakes up, it's already too late...",
-			videoUrl: "https://drive.google.com/file/d/1LNKf5PUpwAADvkCz8OfU3W1CE-jTTR89/preview",
-			message: "NetflixWatch"
+			videoUrl: "https://drive.google.com/file/d/1LNKf5PUpwAADvkCz8OfU3W1CE-jTTR89/preview"
 		},
 		S2E18: {
 			number: 18,
 			title: "Who's the Real Heartsping?",
 			description: "Buddyping is having fun duplicating everything in the bakery. And when Heartsping tries to find him, she gets duplicated into her twin... Good luck finding who's the real one!",
-			videoUrl: "https://drive.google.com/file/d/1-tBRiQW38ex0P1zjOq_pfrdSkglM72lf/preview",
-			message: "NetflixWatch"
+			videoUrl: "https://drive.google.com/file/d/1-tBRiQW38ex0P1zjOq_pfrdSkglM72lf/preview"
 		},
 		S2E19: {
 			number: 19,
 			title: "Find the Gift Box!",
 			description: "Giftping likes to give gifts to everyone! But when she gets chased by Romi, instead of taking her prop, she takes another gift and runs... She will have to recover her magic prop, but how?",
-			videoUrl: "https://drive.google.com/file/d/1lDednvexvie6hcAt3nnRbn5J1L9Ed0tL/preview",
-			message: "NetflixWatch"
+			videoUrl: "https://drive.google.com/file/d/1lDednvexvie6hcAt3nnRbn5J1L9Ed0tL/preview"
 		},
 		S2E20: {
 			number: 20,
 			title: "Where's My Muse, Sculpt?",
 			description: "Sculptping tries to sculpt a Teenieping of the Jewel Forest, but he can't remember her. Later, his statue is found by the Teeniepings, who take it at the Teenieping School. But suddenly, the statue starts to move!",
-			videoUrl: "https://drive.google.com/file/d/1HffFxgXZ5bX079r-TqXJ8KbGubfioRZd/preview",
-			message: "NetflixWatch"
+			videoUrl: "https://drive.google.com/file/d/1HffFxgXZ5bX079r-TqXJ8KbGubfioRZd/preview"
 		},
 		S2E21: {
 			number: 21,
 			title: "A Perilous Picnic",
 			description: "Jennie invites everyone to go to a picnic, and Heartsping, who has a bad feeling about this, is forced to come. But while everyone is having fun, Maskping suddenly appears!",
-			videoUrl: "https://drive.google.com/file/d/1Ra1DRNEnjBYytqRfo_38b0fMcIy-m7wJ/preview",
-			message: "NetflixWatch"
+			videoUrl: "https://drive.google.com/file/d/1Ra1DRNEnjBYytqRfo_38b0fMcIy-m7wJ/preview"
 		},
 		S2E22: {
 			number: 22,
 			title: "Twirl Whirl Teenieping Ballet",
 			description: "When Princess Ruby approaches Balletping, the latter reveals her secret: she wants to have a friend to make a ballet with them. But then, she hears about the Teenieping Ballet...",
-			videoUrl: "https://drive.google.com/file/d/19r0hMYnmpyldOnjC0j3GQNL037qKPmM4/preview",
-			message: "NetflixWatch"
+			videoUrl: "https://drive.google.com/file/d/19r0hMYnmpyldOnjC0j3GQNL037qKPmM4/preview"
 		},
 		S2E23: {
 			number: 23,
 			title: "Wonderping, the Hero Teenieping",
 			description: "Wonderping, a self-proclaimed heroine, is always here to help others! Everyone is looking for her, but when she appears, she gets Princess Ruby into a lot of trouble!",
-			videoUrl: "https://drive.google.com/file/d/1TFIFLuEQ-4caA7tMvgrXddA5v4zD4uNx/preview",
-			message: "NetflixWatch"
+			videoUrl: "https://drive.google.com/file/d/1TFIFLuEQ-4caA7tMvgrXddA5v4zD4uNx/preview"
 		},
 		S2E24: {
 			number: 24,
 			title: "Teenieping School Graduation",
 			description: "Since all the Jewel Teeniepings are now caught, a graduation ceremony is organized at the Teenieping School. Everyone joins, even Jennie and the Royal Teeniepings! Let the party begin!",
-			videoUrl: "https://drive.google.com/file/d/12HXsCfx3GnCh9Y19y7odNLr2Fad1oAYG/preview",
-			message: "NetflixWatch"
+			videoUrl: "https://drive.google.com/file/d/12HXsCfx3GnCh9Y19y7odNLr2Fad1oAYG/preview"
 		},
 		S2E25: {
 			number: 25,
 			title: "Heartsping in Danger",
 			description: "Heartsping has been made prisoner by Maskping! To save them, Romi, helped by Jennie, go to the Emotions Kingdom and they promise to save Heartsping. Meanwhile, the latter discovers in her hiding place something very strange...",
-			videoUrl: "https://drive.google.com/file/d/1IsGPpZzNWxDMzbse5IN7fv1XVNiA3aGZ/preview",
-			message: "NetflixWatch"
+			videoUrl: "https://drive.google.com/file/d/1IsGPpZzNWxDMzbse5IN7fv1XVNiA3aGZ/preview"
 		},
 		S2E26: {
 			number: 26,
 			title: "The Jewel Forest is Restored",
 			description: "After defeating Maskping, Romi finally discovers the whole truth... The story of her friend who stayed in the shadow for way too long... But the battle is not over yet.",
-			videoUrl: "https://drive.google.com/file/d/1qveMcNSUn_ZVEESCf0hEuE1nbIi5SdRa/preview",
-			message: "NetflixWatch"
+			videoUrl: "https://drive.google.com/file/d/1qveMcNSUn_ZVEESCf0hEuE1nbIi5SdRa/preview"
 		}
 	}
 };
 
 var messages = {
-	"NetflixWatch": [
+	"Season 2": [
 		"Where to watch Twinkle Catch! Teenieping",
 		"This episode was taken from Netflix America by @Trueping. If you have Netflix America, you can also watch the English dub there as it would allow you to financially support the show's creators!"
+	],
+	"Season 3": [
+		"We need translation!",
+		"This season's English dub is not yet available, but meanwhile, you can watch it in Korean! If you speak Korean and would like to help make the subtitles, please contact the admins of the Catch! Teenieping Wiki."
+	],
+	"Season 4": [
+		"We need translation!",
+		"This season's English dub is not yet available, but meanwhile, you can watch it in Korean! If you speak Korean and would like to help make the subtitles, please contact the admins of the Catch! Teenieping Wiki."
 	]
 };
